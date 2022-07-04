5abe48a5fb8ccbf62340d4a8c979e03273c439e4
Jiekai <s1082199@gm.pu.edu.tw>
2022-07-04 19:26:43 +0800

    Feat : 更改了一些東西(目前還沒弄功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,20 +1,26 @@
 <template>
+
   <div>
     <h2>登入</h2>
     <el-form @submit.prevent="login">
       <div class="form-group">
         <el-input v-model="name" placeholder="帳號" />
-        <el-input v-model="password" placeholder="密碼" />
+        <el-input v-model="password" placeholder="密碼, 至少有8位" />
         <el-button plain type="primary" native-type="submit">登入</el-button>
       </div>
     </el-form>
   </div>
+
 </template>
 
 <script>
+
 export default {
   data() {
-    return { name: "", password: "" };
+    return {
+      name: "",
+      password: "",
+    };
   },
   methods: {
     login() {
@@ -29,4 +35,19 @@ export default {
     },
   },
 };
+
 </script>
+
+<style>
+
+*{
+  font-family:微軟正黑體;  
+}
+h2, #inputUsername, #inputPassword{
+  width: 200px;
+  height: 20px;
+  margin: 10px;
+  color: #23995c;
+}
+
+</style>
