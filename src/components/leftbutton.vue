dfc6148586e6d01f4eb0b6f5c5a14524524ab650
fanwei <s1080323@gm.pu.edu.tw>
2022-07-12 01:59:07 +0800

    Feat : 新增1. 左側按鈕、2. 右側懸浮按鈕、3. 相關度排行框

---
+++ src/components/leftbutton.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="LB1">
+    <el-main><el-button>默认按钮</el-button></el-main>
+    <el-main><el-button>默认按钮</el-button></el-main>
+    <el-main><el-button>默认按钮</el-button></el-main>
+    </div>
+</template>
+<script>
+
+</script>
+
+<style>
+.LB1 {
+    float: left;
+    width: 150px;
+    height: 50px;        
+    line-height: 80px;
+    
+}
+</style>
---
+++ src/components/rightsuspendbutton.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="RB1">
+        <el-button>SU3CL3RWEER</el-button>
+    </div>
+    <div class="RB2">
+        <el-button >SU3CL3RWEER</el-button>
+    </div>
+    <div class="RB3">
+        <el-button>SU3CL3RWEER</el-button>
+    </div>
+    <div class="RB4">
+        <el-button>SU3CL3RWEER</el-button>
+    </div>
+    <div class="RB5">
+        <el-button>SU3CL3RWEER</el-button>
+    </div>
+    <div class="RB6">
+        <el-button>SU3CL3RWEER</el-button>
+    </div>
+    <div class="RB7">
+        <el-button>SU3CL3RWEER</el-button>
+    </div>
+    <div class="RB8">
+        <el-button>SU3CL3RWEER</el-button>
+    </div>
+    <div class="picture">
+        <img alt="tsmc logo" src="../assets/tsmc.png" />
+    </div>
+</template>
+<script>
+
+</script>
+
+<style>
+.RB1{
+    position: absolute;
+    top: 150px;
+    width: 700px;
+    left: 450px;
+}
+.RB2{
+    position: absolute;
+    top: 300px;
+    width: 700px;
+    left: 250px;
+}
+.RB3{
+    position: absolute;
+    top: 530px;
+    width: 700px;
+    left: 250px;
+}
+.RB4{
+    position: absolute;
+    top: 700px;
+    width: 700px;
+    left: 450px;
+}
+.RB5{
+    position: absolute;
+    top: 700px;
+    width: 700px;
+    left: 1000px;
+}
+.RB6{
+    position: absolute;
+    top: 530px;
+    width: 700px;
+    left: 1200px;
+}
+.RB7{
+    position: absolute;
+    top: 300px;
+    width: 700px;
+    left: 1200px;
+}
+.RB8{
+    position: absolute;
+    top: 150px;
+    width: 700px;
+    left: 1000px;
+}
+.picture{
+    position: absolute;
+    top: 300px;
+    width: 700px;
+    left: 700px;
+}
+</style>
