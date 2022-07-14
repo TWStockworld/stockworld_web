f9b0592fd44a3e3f4cba5f854d1420b4c84d71b5
fanwei <s1080323@gm.pu.edu.tw>
2022-07-15 00:18:19 +0800

    Feat : 新增 1. 點擊懸浮按鈕後會跳出排行框 2. 台積電標誌

--- src/components/rightsuspendbutton.vue
+++ src/components/rightsuspendbutton.vue
@@ -1,33 +1,49 @@
 <template>
     <div class="RB1">
-        <el-button>SU3CL3RWEER</el-button>
+        <el-button  @click="dialogTableVisible = true">SU3CL3RWEER</el-button>
+            <el-dialog v-model="dialogTableVisible" title="相關度排行"><correlation /></el-dialog>
     </div>
     <div class="RB2">
-        <el-button >SU3CL3RWEER</el-button>
+        <el-button  @click="dialogTableVisible = true">SU3CL3RWEER</el-button>
     </div>
     <div class="RB3">
-        <el-button>SU3CL3RWEER</el-button>
+        <el-button  @click="dialogTableVisible = true">SU3CL3RWEER</el-button>
     </div>
     <div class="RB4">
-        <el-button>SU3CL3RWEER</el-button>
+        <el-button  @click="dialogTableVisible = true">SU3CL3RWEER</el-button>
+            
     </div>
     <div class="RB5">
-        <el-button>SU3CL3RWEER</el-button>
+        <el-button  @click="dialogTableVisible = true">SU3CL3RWEER</el-button>
+            
     </div>
     <div class="RB6">
-        <el-button>SU3CL3RWEER</el-button>
+       <el-button  @click="dialogTableVisible = true">SU3CL3RWEER</el-button>
+            
     </div>
     <div class="RB7">
-        <el-button>SU3CL3RWEER</el-button>
+        <el-button  @click="dialogTableVisible = true">SU3CL3RWEER</el-button>
+            
     </div>
     <div class="RB8">
-        <el-button>SU3CL3RWEER</el-button>
+        <el-button  @click="dialogTableVisible = true">SU3CL3RWEER</el-button>
     </div>
     <div class="picture">
         <img alt="tsmc logo" src="../assets/tsmc.png" />
     </div>
 </template>
 <script>
+import correlation from "@/components/correlation.vue"
+export default {
+    components:{
+        correlation,
+    },
+    data() {
+        return{
+            dialogTableVisible: false,
+        }
+    }
+}
 
 </script>
 
