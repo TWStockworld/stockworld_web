39571e8380dc5fea7f15c882930b2dd97a0c7152
Jiekai <s1082199@gm.pu.edu.tw>
2022-07-14 20:20:06 +0800

    Feat : 把register的顯示錯誤改成了會顯示紅色的訊息框錯誤

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,91 +1,154 @@
 <template>
-    <div id="container1">
+  <div id="container1">
     <h3>註冊</h3>
     <el-form @submit.prevent="register">
-    <input type="username" id="inputUsername" v-model="name" placeholder="名字"  />
-     <div class="tab"></div>
-     <input type="account" id="inputAccount" v-model="account" placeholder="帳號"  />
-     <div class="tab"></div>
-     <input type="password" id="inputPassword" v-model="password" placeholder="密碼" />
-     <div class="tab"></div>
-     <input type="email" id="inputEmail" v-model="email" placeholder="Email" />
-     <div class="tab"></div>
-    
-        <el-button type="primary" class="submit" @click="register" >註冊</el-button>
-     </el-form> 
-    </div> 
+      <input
+        type="username"
+        id="inputUsername"
+        v-model="name"
+        placeholder="名字"
+        required
+      />
+      <div class="tab"></div>
+      <input
+        type="account"
+        id="inputAccount"
+        v-model="account"
+        placeholder="帳號"
+        required
+      />
+      <div class="tab"></div>
+      <input
+        type="password"
+        id="inputPassword"
+        v-model="password"
+        placeholder="密碼"
+        required
+      />
+      <div class="tab"></div>
+      <input
+        type="email"
+        id="inputEmail"
+        v-model="email"
+        placeholder="Email"
+        required
+      />
+      <div class="tab"></div>
+
+      <el-button type="primary" class="submit" @click="register"
+        >註冊</el-button
+      >
+    </el-form>
+  </div>
 </template>
 
-
 <script>
-
-
+//////
+import { ElMessage } from "element-plus";
+//////
 
 export default {
   data() {
-     return {
-        name: "",
-        account: "",
-        password: "",
-        email: "",     
+    return {
+      name: "",
+      account: "",
+      password: "",
+      email: "",
     };
-  }, 
- // mounted(){
-    
- // },
-  methods:{
-    register(){
+  },
+  // mounted(){
+
+  // },
+  methods: {
+    register() {
+      // if(this.name === ''|| this.account === '' || this.password === '' || this.email === ''){
+      //   alert('名字、帳號、密碼、信箱不能為空')
+      // }else{
+      //   this.axios
+      //   .post("https://stockworld.ddns.net/api/auth/register",{
+      //       name: this.name,
+      //       account: this.account,
+      //       password: this.password,
+      //       email: this.email,
+      //   })
+
+      //   .then((res) => {
+      //     console.log(res);
+      //     alert ('註冊成功')
+      //     if(res.status == '200' ){
+      //       this.$router.push('/pm25')
+      //       }
+      //     })
+
+      //   .catch(function(error) {
+      //     if (error.response) {
+      //       console.log(error.response.status);
+      //        if(error.response.status == '401'){
+      //           alert ('資料填寫錯誤')
+      //         }else if(error.response.status == '402'){
+      //           alert('帳號名稱重複!')
+      //         }
+      //     }
+      //   });
+      // }
+
+      //////
       if(this.name === ''|| this.account === '' || this.password === '' || this.email === ''){
-        alert('名字、帳號、密碼、信箱不能為空')
-      }else{
+        ElMessage.error("名字、帳號、密碼、信箱不能為空")
+      } else {
         this.axios
-        .post("https://stockworld.ddns.net/api/auth/register",{
+          .post("https://stockworld.ddns.net/api/auth/register", {
             name: this.name,
             account: this.account,
             password: this.password,
             email: this.email,
-        })
-          
-        .then((res) => {
-          console.log(res);
-          alert ('註冊成功')
-          if(res.status == '200' ){
-            this.$router.push('/pm25')
-            }
           })
-        
-        .catch(function(error) {
-          if (error.response) { 
-            console.log(error.response.status);
-             if(error.response.status == '401'){
-                alert ('資料填寫錯誤')
-              }else if(error.response.status == '402'){
-                alert('帳號名稱重複!')
+
+          .then((res) => {
+            console.log(res);
+            //////
+            ElMessage({
+              message: "Congrats, this is a success message.",
+              type: "success",
+            });
+            //////
+          })
+
+          .catch(function (error) {
+            if (error.response) {
+              console.log(error.response.status);
+              if (error.response.status == "401") {
+                ElMessage.error("資料填寫錯誤");
+              } else if (error.response.status == "402") {
+                ElMessage({
+                  message: "帳號名稱重複!",
+                  type: "warning",
+                });
               }
-          }
-        });
-      }   
-    },  
- }
-
-}
-
-
-
+            }
+          });
+      }
+      //////
+    },
+  },
+};
 </script>
 
-
 <style>
-*{
-  font-family:微軟正黑體;  
+* {
+  font-family: 微軟正黑體;
 }
-h3, #inputAccount, #inputEmail,#inputUsername, #inputPassword{
+h3,
+#inputAccount,
+#inputEmail,
+#inputUsername,
+#inputPassword {
   width: 200px;
   height: 20px;
   margin: 10px;
   color: #23995c;
 }
-#container1{
+#container1 {
   //margin: 50px;
   padding: 10px;
   width: 230px;
@@ -94,23 +157,23 @@ h3, #inputAccount, #inputEmail,#inputUsername, #inputPassword{
   border-radius: 5px;
   border-top: 10px solid #23995c;
   box-shadow: 0 0px 70px rgba(0, 0, 0, 0.1);
-  
-   /*定位對齊*/
-  position:relative;   
-  margin: auto;
-  top: 100px;
- //text-align:center; 
-}
-.system_name{
+
   /*定位對齊*/
-  position:relative;   
+  position: relative;
   margin: auto;
   top: 100px;
-  text-align:center; 
+  //text-align:center;
+}
+.system_name {
+  /*定位對齊*/
+  position: relative;
+  margin: auto;
+  top: 100px;
+  text-align: center;
 }
 
-.submit{
-  color: white;  
+.submit {
+  color: white;
   background: #23995c;
   width: 200px;
   height: 30px;
@@ -120,15 +183,13 @@ h3, #inputAccount, #inputEmail,#inputUsername, #inputPassword{
   border: 0px;
 }
 
-.submit:hover{
+.submit:hover {
   background: #219e53;
 }
-input{
+input {
   padding: 5px;
-  border: none; 
-  border:solid 1px #ccc;
+  border: none;
+  border: solid 1px #ccc;
   border-radius: 5px;
 }
-
-
 </style>
