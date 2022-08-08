b3cbab4456da7c2db122f4f6e8502a9fec7127cd
fanwei <s1080323@gm.pu.edu.tw>
2022-08-08 21:52:53 +0800

    Fix

---
+++ src/components/Leftbutton.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="LB1">
+    <el-main
+      ><el-button
+        @click="ranking"
+        style="width: 100%; height: 50px; font-size: 19px"
+        >關係度總排行</el-button
+      ></el-main
+    >
+    <el-main
+      ><el-button
+        @click="tsmc"
+        style="width: 100%; height: 50px; font-size: 20px"
+        >台積電</el-button
+      ></el-main
+    >
+    <el-main
+      ><el-button style="width: 100%; height: 50px; font-size: 20px"
+        >鴻海電動車</el-button
+      ></el-main
+    >
+    <el-main
+      ><el-button style="width: 100%; height: 50px; font-size: 20px"
+        >電動車</el-button
+      ></el-main
+    >
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    ranking() {
+      this.$router.push("/ranking");
+    },
+    tsmc() {
+      this.$router.push("/tsmc");
+    },
+    sort() {
+      this.$router.push("/sort");
+    },
+  },
+};
+</script>
+
+<style>
+.LB1 {
+  position: absolute;
+  left: 5%;
+  width: 8%;
+  line-height: 100px;
+}
+</style>
