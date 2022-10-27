53248b4288ead97423743ff52ecee54106d8dc0b
Jiekai <s1082199@gm.pu.edu.tw>
2022-10-27 21:40:29 +0800

    Feat : stockkind取道api資料，sortview用v-for產生button

--- src/components/Stockkind.vue
+++ src/components/Stockkind.vue
@@ -1,39 +1,33 @@
 <template>
-  <div class="stockkind_computer" :class="[ this.$route.path=='/ranking' ? 'move': '']">
+  <div class="stockkind_computer" :class="[this.$route.path == '/ranking' ? 'move' : '']">
     <el-row>
       <el-col :lg="12" :sm="24" :xs="24">
-        <el-row class="AllrankData_A1">
-          <el-table :data="AllrankData_A1_Up">
-            <el-table-column prop="rank" label="排行" sortable />
+        <el-row class="AllrankData_A1" border stripe height="200" :default-sort="{ prop: 'rank' }">
+          <el-table :data="AllrankData_A1_UP">
+            <el-table-column prop="order" label="排行" sortable />
 
-            <el-table-column prop="code1" label="代號" />
+            <el-table-column prop="stockA_name" label="股票A" />
 
-            <el-table-column prop="name1" label="名稱" />
+            <el-table-column prop="stockB_name" label="股票B" />
 
-            <el-table-column prop="VS" label="VS" />
+            <el-table-column prop="diff" label="天數" />
 
-            <el-table-column prop="code2" label="代號" />
+            <el-table-column prop="up" label="機率" />
 
-            <el-table-column prop="name2" label="名稱" />
-
-            <el-table-column prop="up" label="漲" />
-
-            <el-table-column prop="follow" label="追蹤" width="180" />
+            <el-table-column prop="follow" label="追蹤" />
           </el-table>
         </el-row>
         <el-row class="AllrankData_A2">
-          <el-table :data="AllrankData_A2_Up">
-            <el-table-column prop="rank" label="排行" sortable />
+          <el-table :data="probability_up">
+            <el-table-column prop="order" label="排行" sortable />
 
-            <el-table-column prop="code1" label="代號" />
+            <el-table-column prop="stockA_id" label="代號" />
 
-            <el-table-column prop="name1" label="名稱" />  
+            <el-table-column prop="stockA_name" label="名稱" />
 
-            <el-table-column prop="VS" label="VS" />
+            <el-table-column prop="stockA_id" label="代號" />
 
-            <el-table-column prop="code2" label="代號" />
-
-            <el-table-column prop="name2" label="名稱" />
+            <el-table-column prop="stockB_name" label="名稱" />
 
             <el-table-column prop="up" label="漲" />
 
@@ -42,47 +36,43 @@
         </el-row>
       </el-col>
       <el-col :lg="12" :sm="24" :xs="24">
-        <el-row class="AllrankData_B1">
-          <el-table :data="AllrankData_B1_Down">
-            <el-table-column prop="rank2" label="排行" sortable />
+        <el-row class="AllrankData_B1" border stripe height="200" :default-sort="{ prop: 'rank' }">
+          <el-table :data="AllrankData_B1_DOWN">
+            <el-table-column prop="order" label="排行" sortable />
 
-            <el-table-column prop="code3" label="代號" />
+            <el-table-column prop="stockA_name" label="股票A" />
 
-            <el-table-column prop="name3" label="名稱" />
+            <el-table-column prop="stockB_name" label="股票B" />
 
-            <el-table-column prop="VS" label="VS" />
+            <el-table-column prop="diff" label="天數" />
 
-            <el-table-column prop="code4" label="代號" />
+            <el-table-column prop="up" label="機率" />
 
-            <el-table-column prop="name4" label="名稱" />
-
-            <el-table-column prop="down" label="跌" />
-
-            <el-table-column prop="follow" label="追蹤" width="180" />
+            <el-table-column prop="follow" label="追蹤" />
           </el-table>
         </el-row>
         <el-row class="AllrankData_B2">
-          <el-table :data="AllrankData_B2_Down">
-            <el-table-column prop="rank2" label="排行" sortable />
+          <el-table :data="probability_down">
+            <el-table-column prop="order" label="排行" sortable />
 
-            <el-table-column prop="code3" label="代號" />
+            <el-table-column prop="stockA_id" label="代號" />
 
-            <el-table-column prop="name3" label="名稱" />
+            <el-table-column prop="stockA_name" label="名稱" />
 
-            <el-table-column prop="VS" label="VS" />
+            <el-table-column prop="stockA_id" label="代號" />
 
-            <el-table-column prop="code4" label="代號" />
+            <el-table-column prop="stockB_name" label="名稱" />
 
-            <el-table-column prop="name4" label="名稱" />
+            <el-table-column prop="diff" label="天數" />
 
             <el-table-column prop="down" label="跌" />
 
-            <el-table-column prop="follow" label="追蹤" width="180" />
+            <el-table-column prop="follow" label="追蹤" />
           </el-table>
         </el-row>
       </el-col>
     </el-row>
-    
+
   </div>
 </template>
 <script>
@@ -91,53 +81,21 @@ export default {
   name: "Stockkind",
   data() {
     return {
-      rank: "",
-      code1: "",
-      name1: "",
-      code2: "",
-      name2: "",
-      up: "",
-      rank2: "",
-      code3: "",
-      name3: "",
-      code4: "",
-      name4: "",
-      down: "",
+      probability_up:[],
+      probability_down:[],
     };
   },
   mounted() {
     this.axios
-      .get("https://stock.bakerychu.com/api/stock/get_all_stock_probability"), {
-        rank: this.probability_up.order,
-        code1: this.probability_up.stockA_id,
-        name1: this.probability_up.stockA_name,
-        code2: this.probability_up.stockB_id,
-        name2: this.probability_up.stockB_name,
-        up: this.probability_up.up,
-        rank2: this.probability_down.order,
-        code3: this.probability_down.stockA_id,
-        name3: this.probability_down.stockA_name,
-        code4: this.probability_down.stockB_id,
-        name4: this.probability_down.stockB_name,
-        down: this.probability_down.down,
-      }
-      .then((res) => {
-        console.log(res.data);
-        this.probability_up.order = res.data.order;
-        this.probability_up.stockA_id = res.data.stockA_id;
-        this.probability_up.stockA_name = res.data.stockA_name;
-        this.probability_up.stockB_id = res.data.stockB_id;
-        this.probability_up.stockB_name = res.data.stockB_name;
-        this.probability_up.up = res.data.up;
-        this.probability_down.order = res.data.order;
-        this.probability_down.stockA_id = res.data.stockA_id;
-        this.probability_down.stockA_name = res.data.stockA_name;
-        this.probability_down.stockB_id = res.data.stockB_id;
-        this.probability_down.stockB_name = res.data.stockB_name;
-        this.probability_down.down = res.data.down;
-      })
+      .get("https://stock.bakerychu.com/api/stock/get_all_stock_probability")
+        .then((res) => {
+          console.log(res.data.probability_up,res.data.probability_down);
+          this.probability_up=res.data.probability_up;
+          this.probability_down=res.data.probability_down;
+
+        })
   }
-  
+
 };
 </script>
 <style scoped>
@@ -150,20 +108,25 @@ export default {
     left: 25%;
     font-size: 20px;
   }
+
   .AllrankData_A1 {
-    top:0%;
+    top: 0%;
   }
+
   .AllrankData_A2 {
-    top:10%;
+    top: 10%;
   }
+
   .AllrankData_B1 {
-    top:0%;
-    left:25%;
+    top: 0%;
+    left: 25%;
   }
+
   .AllrankData_B2 {
-    top:10%;
-    left:25%;
+    top: 10%;
+    left: 25%;
   }
+
   .stockkind_cellphone {
     display: none;
   }
@@ -196,6 +159,7 @@ export default {
     left: 15%;
     font-size: 20px; */
   }
+
   .move1 {
     animation: movepoint2 2s ease-in-out;
   }
@@ -211,40 +175,6 @@ export default {
 
   }
 }
-/*
-mounted() {
-    this.axios
-      .get(url), {
-        rank: this.order_up,
-        code1: this.stockA_id_up,
-        name1: this.stockA_name_up,
-        code2: this.stockB_id_up,
-        name2: this.stockB_name_up,
-        up: this.up,
-        rank2: this.order_down,
-        code3: this.stockA_id_down,
-        name3: this.stockA_name_down,
-        code4: this.stockB_id_down,
-        name4: this.stockB_name_down,
-        down: this.down,
-      }
-      .then((res) => {
-        console.log(res.data);
-        this.order_up = res.data.probability_up.order;
-        this.stockA_id_up = res.data.probability_up.stockA_id;
-        this.stockA_name_up = res.data.probability_up.stockA_name;
-        this.stockB_id_up = res.data.probability_up.stockB_id;
-        this.stockB_name_up = res.data.probability_up.stockB_name;
-        this.up = res.data.probability_up.up;
-        this.order_down = res.data.probability_down.order;
-        this.stockA_id_down = res.data.probability_down.stockA_id;
-        this.stockA_name_down = res.data.probability_down.stockA_name;
-        this.stockB_id_down = res.data.probability_down.stockB_id;
-        this.stockB_name_down = res.data.probability_down.stockB_name;
-        this.down = res.data.probability_down.down;
-      })
-  }
-*/ 
 </style>
 
 
--- src/views/SortView.vue
+++ src/views/SortView.vue
@@ -1,20 +1,77 @@
 <template>
   <div class="sort_computersize">
     <div>
-    <el-button class="sb1" @click="kindtable = true">上市</el-button>
+      <el-button class="sb1" @click="kindtable = true">上市</el-button>
     </div>
     <div>
       <el-dialog v-model="kindtable" width="50%">
-        <el-button v-for="sort in sorts" :key="sort.id">
-          {{ sort.kind }}
-        </el-button>
+        <el-container>
+          <el-header class="sort_title">台股分類查詢-上市</el-header>
+          <el-main>
+            <el-button v-for="category in categories" :key="category.id">
+              {{ category.category }}
+            </el-button>
+
+          </el-main>
+        </el-container>
+        <!--  先把button寫在上面
+           <el-button class="sort_list" color="#626aef" :dark="isDark" plain>水泥</el-button>
+        -->
       </el-dialog>
     </div>
     <div>
-      <el-button class="sb2">上櫃</el-button>
-      <el-button class="sb3">概念股</el-button>
-      <el-button class="sb4">集團</el-button>
-      <el-button class="sb5">電子股</el-button>
+      <el-button class="sb2" @click="kindtable2 = true">上櫃</el-button>
+    </div>
+    <div>
+      <el-dialog v-model="kindtable2" width="50%">
+        <el-container>
+          <el-header class="sort_title">台股分類查詢-上櫃</el-header>
+          <el-main>
+            <el-button v-for="category in categories" :key="category.id">
+              {{ category.category }}
+            </el-button>
+          </el-main>
+        </el-container>
+      </el-dialog>
+    </div>
+    <div>
+      <el-button class="sb3" @click="kindtable3 = true">概念股</el-button>
+    </div>
+    <div>
+      <el-dialog v-model="kindtable3" width="50%">
+        <el-container>
+          <el-header class="sort_title">台股分類查詢-概念股</el-header>
+          <el-main>
+
+          </el-main>
+        </el-container>
+      </el-dialog>
+    </div>
+    <div>
+      <el-button class="sb4" @click="kindtable4 = true">集團</el-button>
+    </div>
+    <div>
+      <el-dialog v-model="kindtable4" width="50%">
+        <el-container>
+          <el-header class="sort_title">台股分類查詢-集團</el-header>
+          <el-main>
+
+          </el-main>
+        </el-container>
+      </el-dialog>
+    </div>
+    <div>
+      <el-button class="sb5" @click="kindtable5 = true">電子股</el-button>
+    </div>
+    <div>
+      <el-dialog v-model="kindtable5" width="50%">
+        <el-container>
+          <el-header class="sort_title">台股分類查詢-電子股</el-header>
+          <el-main>
+
+          </el-main>
+        </el-container>
+      </el-dialog>
     </div>
   </div>
 
@@ -28,7 +85,7 @@
             </div>
           </template>
           <div v-for="o in 4" :key="o" class="text item">
-            {{'列表内容 ' + o }}
+            {{ '列表内容 ' + o }}
           </div>
         </el-card>
       </li>
@@ -41,7 +98,7 @@
             </div>
           </template>
           <div v-for="o in 4" :key="o" class="text item">
-            {{'列表内容 ' + o }}
+            {{ '列表内容 ' + o }}
           </div>
         </el-card>
       </li>
@@ -54,7 +111,7 @@
             </div>
           </template>
           <div v-for="o in 4" :key="o" class="text item">
-            {{'列表内容 ' + o }}
+            {{ '列表内容 ' + o }}
           </div>
         </el-card>
       </li>
@@ -67,7 +124,7 @@
             </div>
           </template>
           <div v-for="o in 4" :key="o" class="text item">
-            {{'列表内容 ' + o }}
+            {{ '列表内容 ' + o }}
           </div>
         </el-card>
       </li>
@@ -80,7 +137,7 @@
             </div>
           </template>
           <div v-for="o in 4" :key="o" class="text item">
-            {{'列表内容 ' + o }}
+            {{ '列表内容 ' + o }}
           </div>
         </el-card>
       </li>
@@ -92,103 +149,142 @@
 export default {
   data() {
     return {
-      sorts: [],
+      categories: [],
       kindtable: false,
+      kindtable2: false,
+      kindtable3: false,
+      kindtable4: false,
+      kindtable5: false,
+
     };
   },
-  methods: {},
+  mounted() {
+    this.axios
+      .get("/api/stock/get_stock_category")
+      .then((res) => {
+        console.log(res.data.success);
+        this.categories = res.data.success;
+
+
+      })
+  }
 };
 </script>
 <style scoped>
-  @media only screen and (max-width: 768px) {
-    .sort_computersize {
-      display: none;
-    }
-    .sort_name {
-      font-size: 1.5em;
-    }
-    .text {
+@media only screen and (max-width: 768px) {
+  .sort_computersize {
+    display: none;
+  }
+
+  .sort_name {
+    font-size: 1.5em;
+  }
+
+  .text {
     font-size: 14px;
-    }
-    .item {
-      margin-bottom: 18px;
-    }
-    .clearfix:before,
-    .clearfix:after {
-      display: table;
-      content: "";
-    }
-    .clearfix:after {
-      clear: both
-    }
-    .box-card {
-      width: 90%;
-    }
-    .sort_cellphonesize_ul {
-      list-style-type: none;
-      height: 85vh;
-      overflow-y: scroll;
-    }
-    .tab11{
-      height: 1%;
-    }
-    .sort_cellphonesize {
-      width: 90%;
-      /* margin: auto; */
-      margin-top: 6%;
-      margin-right: auto;
-      margin-bottom: auto;
-      margin-left: 8%;
-    }
   }
-  @media only screen and (min-width: 768px) {
-    .sort_cellphonesize {
-      display: none;
-    }
-    .sb1 {
-      position: absolute;
-      height: 26%;
-      width: 13%;
-      border-radius: 50%;
-      top: 20%;
-      left: 10%;
-      font-size: 100px;
-    }
-    .sb2 {
-      position: absolute;
-      height: 20%;
-      width: 10%;
-      border-radius: 50%;
-      top: 55%;
-      left: 28%;
-      font-size: 90px;
-    }
-    .sb3 {
-      position: absolute;
-      height: 30%;
-      width: 15%;
-      border-radius: 50%;
-      top: 20%;
-      left: 40%;
-      font-size: 90px;
-    }
-    .sb4 {
-      position: absolute;
-      height: 20%;
-      width: 10%;
-      border-radius: 50%;
-      top: 50%;
-      left: 55%;
-      font-size: 90px;
-    }
-    .sb5 {
-      position: absolute;
-      height: 26%;
-      width: 13%;
-      border-radius: 50%;
-      top: 20%;
-      left: 70%;
-      font-size: 80px;
-    }
+
+  .item {
+    margin-bottom: 18px;
   }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 90%;
+  }
+
+  .sort_cellphonesize_ul {
+    list-style-type: none;
+    height: 85vh;
+    overflow-y: scroll;
+  }
+
+  .tab11 {
+    height: 1%;
+  }
+
+  .sort_cellphonesize {
+    width: 90%;
+    /* margin: auto; */
+    margin-top: 6%;
+    margin-right: auto;
+    margin-bottom: auto;
+    margin-left: 8%;
+  }
+}
+
+@media only screen and (min-width: 768px) {
+  .sort_cellphonesize {
+    display: none;
+  }
+
+  .sort_list {
+    border-radius: 10%;
+    font-size: 20px;
+  }
+
+  .sort_title {
+    font-size: 50px;
+    margin: auto;
+  }
+
+  .sb1 {
+    position: absolute;
+    height: 26%;
+    width: 13%;
+    border-radius: 50%;
+    top: 20%;
+    left: 10%;
+    font-size: 100px;
+  }
+
+  .sb2 {
+    position: absolute;
+    height: 20%;
+    width: 10%;
+    border-radius: 50%;
+    top: 55%;
+    left: 28%;
+    font-size: 90px;
+  }
+
+  .sb3 {
+    position: absolute;
+    height: 30%;
+    width: 15%;
+    border-radius: 50%;
+    top: 20%;
+    left: 40%;
+    font-size: 90px;
+  }
+
+  .sb4 {
+    position: absolute;
+    height: 20%;
+    width: 10%;
+    border-radius: 50%;
+    top: 50%;
+    left: 55%;
+    font-size: 90px;
+  }
+
+  .sb5 {
+    position: absolute;
+    height: 26%;
+    width: 13%;
+    border-radius: 50%;
+    top: 20%;
+    left: 70%;
+    font-size: 80px;
+  }
+}
 </style>
