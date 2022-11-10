7d08743fa00480c23233d57d8aac0ef025f93f6a
Colten <likeyou600@gmail.com>
2022-11-11 00:31:54 +0800

    Feat : 輸出隱藏Log

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,8 @@
-const productPlugins = []; // 專案釋出階段所需要的 babel 外掛
-
+const removeConsolePlugin = [];
 if (process.env.NODE_ENV === "production") {
-  // 釋出模式下使用外掛
-  productPlugins.push("transform-remove-console");
+  removeConsolePlugin.push("transform-remove-console");
 }
-
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: removeConsolePlugin,
 };
